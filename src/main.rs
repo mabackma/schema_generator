a8f03cda93ff71fde69625f956868322f8724aae
mabackma <marc.backman@edu.lapinamk.fi>
2024-11-16 23:47:55 +0200

    creating structs

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use quick_xml::reader::Reader;
 use quick_xml::events::Event::{Start, End, Eof};
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{self, Read};
+use std::io::{self, Read, Write};
+use xml_schema_generator::{into_struct, Options};
 
 #[derive(Debug)]
 struct XMLField {
     name: String,
-    field_type: String, // "String" for simple fields or a struct name for nested ones
+    field_type: String,
 }
 
 #[derive(Debug)]
@@ -19,6 +20,14 @@ struct XMLStruct {
 fn main() {    
     let xml_string = read_xml_file("forestpropertydata.xml");
     let mut reader = Reader::from_str(&xml_string);
+
+/*     if let Ok(root) = into_struct(&mut reader) {
+        let struct_as_string = root.to_serde_struct(&Options::quick_xml_de());
+
+        // save this result as a .rs file and use it to (de)serialize an XML document with quick_xml::de::from_str(xml)
+        let mut struct_file = File::create("src/forest_property_data.rs").unwrap();
+        struct_file.write_all(&struct_as_string.as_bytes()).unwrap();
+    } */
 
     let mut stack: Vec<XMLStruct> = Vec::new(); // Stack of structs being constructed
     let mut structs: HashMap<String, XMLStruct> = HashMap::new(); // Finalized structs
@@ -34,11 +43,10 @@ fn main() {
                     fields: Vec::new(),
                 };
 
-                // If there's a parent struct, check if this field is already present
+                // If there's a parent struct, add this struct as a field to it
                 if let Some(parent_struct) = stack.last_mut() {
-                    // Check if the parent already contains a field with the same name
-                    if parent_struct.fields.iter().all(|field| field.name != element_name) {
-                        // Add the current struct as a field to the parent struct
+                    // Check that the parent doesn't contain a field with the same name
+                    if !parent_struct.fields.iter().any(|field| field.name == element_name) {
                         parent_struct.fields.push(XMLField {
                             name: element_name.clone(),
                             field_type: element_name.clone(), // Use the same name as struct type
@@ -64,7 +72,7 @@ fn main() {
                     if let Some(existing_struct) = structs.get_mut(&completed_struct.name.clone()) {
                         // Merge fields: add only new unique fields
                         for field in completed_struct.fields {
-                            if existing_struct.fields.iter().all(|f| f.name != field.name) {
+                            if !existing_struct.fields.iter().any(|f| f.name == field.name) {
                                 existing_struct.fields.push(field);
                             }
                         }
@@ -81,9 +89,20 @@ fn main() {
     }
 
     for (name, xml_struct) in &structs {
+
         if xml_struct.fields.len() > 0 {
-            println!("{}\n{:#?}\n", name, xml_struct.fields);
-        }
+            let struct_name = name.split(":").last().unwrap(); 
+            println!("{}", struct_name);
+
+            for field in &xml_struct.fields {
+                let field_type = field.name.split(":").last().unwrap();
+                let field_name = field.name.split(":").next().unwrap().to_owned() + "_" + &field_type.to_lowercase();
+                println!("#[serde(rename = \"{}\", skip_serializing_if = \"Option::is_none\")]", field_type);
+                println!("{}: Option<{}>,", field_name, field_type);
+            }
+            println!("\n");
+        } 
+        
     }
 }
 
---
+++ src/results.rs
@@ -0,0 +1,603 @@
+re:Parcels
+[
+    XMLField {
+        name: "re:Parcel",
+        field_type: "re:Parcel",
+    },
+]
+
+gml:pointProperty
+[
+    XMLField {
+        name: "gml:Point",
+        field_type: "gml:Point",
+    },
+]
+
+op:Assortment
+[
+    XMLField {
+        name: "co:ChangeState",
+        field_type: "co:ChangeState",      
+    },
+    XMLField {
+        name: "op:TreeSpecies",
+        field_type: "op:TreeSpecies",      
+    },
+    XMLField {
+        name: "op:StemType",
+        field_type: "op:StemType",
+    },
+    XMLField {
+        name: "op:AssortmentVolume",       
+        field_type: "op:AssortmentVolume", 
+    },
+]
+
+re:Parcel
+[
+    XMLField {
+        name: "re:ParcelNumber",
+        field_type: "re:ParcelNumber",     
+    },
+    XMLField {
+        name: "st:Stands",
+        field_type: "st:Stands",
+    },
+]
+
+re:RealEstates
+[
+    XMLField {
+        name: "re:RealEstate",
+        field_type: "re:RealEstate",       
+    },
+]
+
+op:Operation
+[
+    XMLField {
+        name: "co:ChangeState",
+        field_type: "co:ChangeState",      
+    },
+    XMLField {
+        name: "co:ChangeTime",
+        field_type: "co:ChangeTime",       
+    },
+    XMLField {
+        name: "op:OperationType",
+        field_type: "op:OperationType",    
+    },
+    XMLField {
+        name: "op:ProposalData",
+        field_type: "op:ProposalData",     
+    },
+    XMLField {
+        name: "op:Cutting",
+        field_type: "op:Cutting",
+    },
+    XMLField {
+        name: "op:Specifications",
+        field_type: "op:Specifications",   
+    },
+    XMLField {
+        name: "op:CompletionData",
+        field_type: "op:CompletionData",   
+    },
+    XMLField {
+        name: "op:OperationInfo",
+        field_type: "op:OperationInfo",    
+    },
+]
+
+ts:TreeStandData
+[
+    XMLField {
+        name: "ts:TreeStandDataDate",      
+        field_type: "ts:TreeStandDataDate",
+    },
+]
+
+op:Specification
+[
+    XMLField {
+        name: "co:ChangeState",
+        field_type: "co:ChangeState",      
+    },
+    XMLField {
+        name: "op:SpecificationCode",      
+        field_type: "op:SpecificationCode",
+    },
+]
+
+op:Specifications
+[
+    XMLField {
+        name: "op:Specification",
+        field_type: "op:Specification",
+    },
+]
+
+ts:TreeStandDataDate
+[
+    XMLField {
+        name: "tst:TreeStrata",
+        field_type: "tst:TreeStrata",
+    },
+    XMLField {
+        name: "tss:TreeStandSummary",
+        field_type: "tss:TreeStandSummary",
+    },
+    XMLField {
+        name: "dts:DeadTreeStrata",
+        field_type: "dts:DeadTreeStrata",
+    },
+]
+
+tss:TreeStandSummary
+[
+    XMLField {
+        name: "co:ChangeState",
+        field_type: "co:ChangeState",
+    },
+    XMLField {
+        name: "tss:MeanAge",
+        field_type: "tss:MeanAge",
+    },
+    XMLField {
+        name: "tss:BasalArea",
+        field_type: "tss:BasalArea",
+    },
+    XMLField {
+        name: "tss:StemCount",
+        field_type: "tss:StemCount",
+    },
+    XMLField {
+        name: "tss:MeanDiameter",
+        field_type: "tss:MeanDiameter",
+    },
+    XMLField {
+        name: "tss:MeanHeight",
+        field_type: "tss:MeanHeight",
+    },
+    XMLField {
+        name: "tss:Volume",
+        field_type: "tss:Volume",
+    },
+    XMLField {
+        name: "tss:VolumeGrowth",
+        field_type: "tss:VolumeGrowth",
+    },
+    XMLField {
+        name: "tss:Value",
+        field_type: "tss:Value",
+    },
+    XMLField {
+        name: "tss:ValueGrowthPercent",
+        field_type: "tss:ValueGrowthPercent",
+    },
+    XMLField {
+        name: "tss:SawLogVolume",
+        field_type: "tss:SawLogVolume",
+    },
+    XMLField {
+        name: "tss:PulpWoodVolume",
+        field_type: "tss:PulpWoodVolume",
+    },
+]
+
+op:CompletionData
+[
+    XMLField {
+        name: "op:CompletionDate",
+        field_type: "op:CompletionDate",
+    },
+]
+
+st:Stands
+[
+    XMLField {
+        name: "st:Stand",
+        field_type: "st:Stand",
+    },
+]
+
+re:RealEstate
+[
+    XMLField {
+        name: "re:MunicipalityNumber",
+        field_type: "re:MunicipalityNumber",
+    },
+    XMLField {
+        name: "re:AreaNumber",
+        field_type: "re:AreaNumber",
+    },
+    XMLField {
+        name: "re:GroupNumber",
+        field_type: "re:GroupNumber",
+    },
+    XMLField {
+        name: "re:UnitNumber",
+        field_type: "re:UnitNumber",
+    },
+    XMLField {
+        name: "re:RealEstateName",
+        field_type: "re:RealEstateName",
+    },
+    XMLField {
+        name: "re:Parcels",
+        field_type: "re:Parcels",
+    },
+]
+
+op:Assortments
+[
+    XMLField {
+        name: "op:Assortment",
+        field_type: "op:Assortment",
+    },
+]
+
+gml:polygonProperty
+[
+    XMLField {
+        name: "gml:Polygon",
+        field_type: "gml:Polygon",
+    },
+]
+
+op:ProposalData
+[
+    XMLField {
+        name: "op:ProposalType",
+        field_type: "op:ProposalType",
+    },
+    XMLField {
+        name: "op:ProposalYear",
+        field_type: "op:ProposalYear",
+    },
+]
+
+dts:DeadTreeStrata
+[
+    XMLField {
+        name: "dts:DeadTreeStratum",
+        field_type: "dts:DeadTreeStratum",
+    },
+]
+
+gml:interior
+[
+    XMLField {
+        name: "gml:LinearRing",
+        field_type: "gml:LinearRing",
+    },
+]
+
+op:Operations
+[
+    XMLField {
+        name: "op:Operation",
+        field_type: "op:Operation",
+    },
+]
+
+st:Identifiers
+[
+    XMLField {
+        name: "st:Identifier",
+        field_type: "st:Identifier",
+    },
+]
+
+gml:Point
+[
+    XMLField {
+        name: "gml:coordinates",
+        field_type: "gml:coordinates",
+    },
+]
+
+gml:Polygon
+[
+    XMLField {
+        name: "gml:exterior",
+        field_type: "gml:exterior",
+    },
+    XMLField {
+        name: "gml:interior",
+        field_type: "gml:interior",
+    },
+]
+
+dts:DeadTreeStratum
+[
+    XMLField {
+        name: "co:ChangeState",
+        field_type: "co:ChangeState",
+    },
+    XMLField {
+        name: "dts:DeadTreeType",
+        field_type: "dts:DeadTreeType",
+    },
+    XMLField {
+        name: "dts:TreeSpecies",
+        field_type: "dts:TreeSpecies",
+    },
+    XMLField {
+        name: "dts:MeanDiameter",
+        field_type: "dts:MeanDiameter",
+    },
+    XMLField {
+        name: "dts:Volume",
+        field_type: "dts:Volume",
+    },
+]
+
+st:SpecialFeature
+[
+    XMLField {
+        name: "co:ChangeState",
+        field_type: "co:ChangeState",
+    },
+    XMLField {
+        name: "sf:FeatureCode",
+        field_type: "sf:FeatureCode",
+    },
+    XMLField {
+        name: "sf:FeatureAdditionalCode",
+        field_type: "sf:FeatureAdditionalCode",
+    },
+]
+
+gml:exterior
+[
+    XMLField {
+        name: "gml:LinearRing",
+        field_type: "gml:LinearRing",
+    },
+]
+
+st:StandBasicData
+[
+    XMLField {
+        name: "co:ChangeState",
+        field_type: "co:ChangeState",
+    },
+    XMLField {
+        name: "co:ChangeTime",
+        field_type: "co:ChangeTime",
+    },
+    XMLField {
+        name: "st:CompleteState",
+        field_type: "st:CompleteState",
+    },
+    XMLField {
+        name: "st:StandNumber",
+        field_type: "st:StandNumber",
+    },
+    XMLField {
+        name: "st:MainGroup",
+        field_type: "st:MainGroup",
+    },
+    XMLField {
+        name: "st:SubGroup",
+        field_type: "st:SubGroup",
+    },
+    XMLField {
+        name: "st:FertilityClass",
+        field_type: "st:FertilityClass",
+    },
+    XMLField {
+        name: "st:SoilType",
+        field_type: "st:SoilType",
+    },
+    XMLField {
+        name: "st:DrainageState",
+        field_type: "st:DrainageState",
+    },
+    XMLField {
+        name: "st:DevelopmentClass",
+        field_type: "st:DevelopmentClass",
+    },
+    XMLField {
+        name: "st:StandQuality",
+        field_type: "st:StandQuality",
+    },
+    XMLField {
+        name: "st:MainTreeSpecies",
+        field_type: "st:MainTreeSpecies",
+    },
+    XMLField {
+        name: "st:Accessibility",
+        field_type: "st:Accessibility",
+    },
+    XMLField {
+        name: "st:StandBasicDataDate",
+        field_type: "st:StandBasicDataDate",
+    },
+    XMLField {
+        name: "st:Area",
+        field_type: "st:Area",
+    },
+    XMLField {
+        name: "gdt:PolygonGeometry",
+        field_type: "gdt:PolygonGeometry",
+    },
+    XMLField {
+        name: "st:StandInfo",
+        field_type: "st:StandInfo",
+    },
+    XMLField {
+        name: "st:AreaDecrease",
+        field_type: "st:AreaDecrease",
+    },
+    XMLField {
+        name: "st:DitchingYear",
+        field_type: "st:DitchingYear",
+    },
+    XMLField {
+        name: "st:StandNumberExtension",
+        field_type: "st:StandNumberExtension",
+    },
+    XMLField {
+        name: "st:Identifiers",
+        field_type: "st:Identifiers",
+    },
+    XMLField {
+        name: "st:CuttingRestriction",
+        field_type: "st:CuttingRestriction",
+    },
+]
+
+st:SpecialFeatures
+[
+    XMLField {
+        name: "st:SpecialFeature",
+        field_type: "st:SpecialFeature",
+    },
+]
+
+st:Stand
+[
+    XMLField {
+        name: "st:StandBasicData",
+        field_type: "st:StandBasicData",
+    },
+    XMLField {
+        name: "ts:TreeStandData",
+        field_type: "ts:TreeStandData",
+    },
+    XMLField {
+        name: "op:Operations",
+        field_type: "op:Operations",
+    },
+    XMLField {
+        name: "st:SpecialFeatures",
+        field_type: "st:SpecialFeatures",
+    },
+]
+
+ForestPropertyData
+[
+    XMLField {
+        name: "re:RealEstates",
+        field_type: "re:RealEstates",
+    },
+]
+
+gml:LinearRing
+[
+    XMLField {
+        name: "gml:coordinates",
+        field_type: "gml:coordinates",
+    },
+]
+
+gdt:PolygonGeometry
+[
+    XMLField {
+        name: "gml:pointProperty",
+        field_type: "gml:pointProperty",
+    },
+    XMLField {
+        name: "gml:polygonProperty",
+        field_type: "gml:polygonProperty",
+    },
+]
+
+tst:TreeStratum
+[
+    XMLField {
+        name: "co:ChangeState",
+        field_type: "co:ChangeState",
+    },
+    XMLField {
+        name: "tst:StratumNumber",
+        field_type: "tst:StratumNumber",
+    },
+    XMLField {
+        name: "tst:TreeSpecies",
+        field_type: "tst:TreeSpecies",
+    },
+    XMLField {
+        name: "tst:Storey",
+        field_type: "tst:Storey",
+    },
+    XMLField {
+        name: "tst:Age",
+        field_type: "tst:Age",
+    },
+    XMLField {
+        name: "tst:BasalArea",
+        field_type: "tst:BasalArea",
+    },
+    XMLField {
+        name: "tst:MeanDiameter",
+        field_type: "tst:MeanDiameter",
+    },
+    XMLField {
+        name: "tst:MeanHeight",
+        field_type: "tst:MeanHeight",
+    },
+    XMLField {
+        name: "co:DataSource",
+        field_type: "co:DataSource",
+    },
+    XMLField {
+        name: "tst:StemCount",
+        field_type: "tst:StemCount",
+    },
+    XMLField {
+        name: "tst:Volume",
+        field_type: "tst:Volume",
+    },
+    XMLField {
+        name: "tst:SawLogPercent",
+        field_type: "tst:SawLogPercent",
+    },
+    XMLField {
+        name: "tst:SawLogVolume",
+        field_type: "tst:SawLogVolume",
+    },
+    XMLField {
+        name: "tst:PulpWoodVolume",
+        field_type: "tst:PulpWoodVolume",
+    },
+    XMLField {
+        name: "tst:VolumeGrowth",
+        field_type: "tst:VolumeGrowth",
+    },
+]
+
+tst:TreeStrata
+[
+    XMLField {
+        name: "tst:TreeStratum",
+        field_type: "tst:TreeStratum",
+    },
+]
+
+op:Cutting
+[
+    XMLField {
+        name: "op:CuttingVolume",
+        field_type: "op:CuttingVolume",
+    },
+    XMLField {
+        name: "op:Assortments",
+        field_type: "op:Assortments",
+    },
+]
+
+st:Identifier
+[
+    XMLField {
+        name: "co:IdentifierType",
+        field_type: "co:IdentifierType",
+    },
+    XMLField {
+        name: "co:IdentifierValue",
+        field_type: "co:IdentifierValue",
+    },
+]
